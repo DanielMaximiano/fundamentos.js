dbdf65d87b7f49748172c6f70eed3149e2465ea3
Daniel Maximiano <danielmaxpaula@gmail.com>
2022-02-23 12:31:45 -0300

    códigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dado 
+// booleanos
+
+// conversão implícita
+// o sinal de igual (=) - 1 igual (=) para atribuir valor uma variável. 2 iguais (==) para comparação pelo valor. 3 iguais (===) compara não só valor mas string. Exemplo: os dois tem se ser string para dar true.
+
+const numero = 456;
+const numeroString = Number ("456a");
+ 
+//Number()
+//String()
+console.log(numero + numeroString)
+
+// conversão explícita
---
+++ truthy_falsy.js
@@ -0,0 +1,26 @@
+// boolean
+const usuarioLogado = true
+const contaPaga = false
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+// console.log(0 == false)
+// console.log(" " == false)
+// console.log(1 == true)
+
+// undefined (uma variável declara mas não tem valor)
+
+// null (um tipo de dado que representa nada (vazio ou nada))
+//typeof é um palavra chave da qual, perguntamos ao javascript qual tipo de dado está sendo guardado nessa variável.
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
