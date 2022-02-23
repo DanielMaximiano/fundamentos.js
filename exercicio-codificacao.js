110e29680a70cb1cf443ed5ace3b9dd14a35f2e0
Daniel Maximiano <danielmaxpaula@gmail.com>
2022-02-23 09:58:56 -0300

    exemplos da aula 1

---
+++ exercicio-codificacao.js
@@ -0,0 +1,25 @@
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+// // teste (Crtl + / = para selecionar tudo e deixar com o (//))
+// const cidade = "belo horizonte";
+// const input = "Belo Horizonte";
+
+// console.log(cidade === input); // false
+
+
+// teste
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
